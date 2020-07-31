9f59701000b7a74a14b86d76494e8669abd8207c
Jill Short <jillian.b.short@gmail.com>
2020-07-31 01:38:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -82,17 +82,6 @@ class TicTacToe
     display_board
   end
   
-#   def turn(board)
-#   puts "Please enter 1-9:"
-#   user_input = gets.strip
-#   index = input_to_index(user_input)
-#   if valid_move?(board, index)
-#       move(board, index, current_player(board))
-#       turn(board)
-#   end
-#   display_board(board)
-# end
-  
   def won?
     WIN_COMBINATIONS.each do |combo|
       #binding.pry
