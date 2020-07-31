0b32e01dabb5f485dede9b9d9de59b9ae55dca45
Jill Short <jillian.b.short@gmail.com>
2020-07-31 01:30:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -74,7 +74,7 @@ class TicTacToe
     user_input = gets
     index = input_to_index(user_input)
     if valid_move?(index)
-      move(index)
+      move(index, token)
       display_board
     else
       puts "Invalid selection, please choose again:"
